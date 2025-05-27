59c35868de9fc42aa186f3eb44bff7b3a9a6bb31
shanda <brucewang147862@gmail.com>
2025-05-27 12:59:53 -0700

    deploy

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000
 connectDB();
 
 app.use(cors({
-    origin: 'http://localhost:5173' , // frontend URL ||process.env.CLIENT_ORIGIN
+    origin: process.env.CLIENT_ORIGIN , // frontend URL ||'http://localhost:5173'
     credentials: true,
 }));
 app.use(morgan("dev"));
